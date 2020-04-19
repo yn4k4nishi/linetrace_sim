f244babc1dff6fcf42cd0da3722cbf49b339a7f6
ynakanishi <yn4k4nishi.kit@gmail.com>
2020-04-19 14:53:42 +0900

    feat : Timer

---
+++ include/timer.h
@@ -0,0 +1,18 @@
+//
+// Created by ynakanishi on 2020/04/19.
+//
+
+#ifndef LINETRACE_SIM_TIMER_H
+#define LINETRACE_SIM_TIMER_H
+
+#include "chrono"
+
+class Timer{
+public:
+    Timer(){}
+    ~Timer(){}
+
+    void start(){}
+};
+
+#endif //LINETRACE_SIM_TIMER_H
