18a242141892d44210c6ee0fac0ed72bfb10ed68
ZyuC <m9t1k3zczc@gmail.com>
2020-04-19 16:58:10 +0900

    robotの位置を計算

--- include/robot.h
+++ include/robot.h
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cmath>
 #include "motor.h"
 
 struct State{
@@ -9,27 +10,40 @@ struct State{
 
 class Robot{
 private:
+    double interval;
     State robot_state;
     Motor motor_r, motor_l;
     int sensor_num;
     std::vector<double> sensor_value;
+    double width;
 
 public:
-    Robot(State robot_state = {0.0, 0.0, 0.0}, int sensor_num = 4) :
-            robot_state(this->robot_state),
-            sensor_num(this->sensor_num),
-            sensor_value(sensor_num){
+    Robot(double interval = 0.01, State robot_state = {0.0, 0.0, 0.0}, int sensor_num = 4, double width = 0.1) :
+            interval(interval),
+            robot_state(robot_state),
+            sensor_num(sensor_num),
+            sensor_value(sensor_num),
+            width(width){
     }
 
 private:
     void update_state(){
-        motor_r.getSpeed();
-        motor_l.getSpeed();
+        double speed_r = motor_r.getSpeed();
+        double speed_l = motor_l.getSpeed();
+
+        double v = (speed_r + speed_l) / 2.0;
+        double omega = (speed_r - speed_l) / width;
+
+        robot_state.x += v * interval * cos(robot_state.theta + omega * interval / 2.0);
+        robot_state.y += v * interval * sin(robot_state.theta + omega * interval / 2.0);
+        robot_state.theta += omega * interval;
         return;
     }
 
 public:
     void update(){
+        motor_r.update();
+        motor_l.update();
         update_state();
         return;
     }
