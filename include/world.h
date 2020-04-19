32d5de951eaf500a10807feefe98d51cd29a7eb3
ynakanishi <yn4k4nishi.kit@gmail.com>
2020-04-19 15:02:16 +0900

    add world class

---
+++ include/world.h
@@ -0,0 +1,18 @@
+//
+// Created by ynakanishi on 2020/04/19.
+//
+
+#ifndef LINETRACE_SIM_WORLD_H
+#define LINETRACE_SIM_WORLD_H
+
+class World{
+public:
+    World(){}
+    ~World(){}
+
+    void setRobot(){}
+    void update(){}
+    void plot(){}
+};
+
+#endif //LINETRACE_SIM_WORLD_H
