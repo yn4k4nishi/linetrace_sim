11cda0ab8b909e8ddf80fd375f354fecfd64371d
ZyuC <m9t1k3zczc@gmail.com>
2020-04-20 13:07:57 +0900

    Sensorクラスの雛形を作成

---
+++ include/sensor.h
@@ -0,0 +1,27 @@
+#include "structs.h"
+
+class Sensor{
+private:
+    Position pos;
+    double value;
+
+public:
+    Sensor():
+            pos({0.0, 0.0}),
+            value(0.0){
+    }
+
+public:
+    void update(){
+        return;
+    }
+
+    void setPosition(Position pos){
+        this->pos = pos;
+        return;
+    }
+
+    double getValue(){
+        return value;
+    }
+};
