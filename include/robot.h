9e7d075d43102ee4db3c8c9e9de539c95666a112
ZyuC <m9t1k3zczc@gmail.com>
2020-04-20 12:13:31 +0900

    姿勢がロボットのなのは自明なので変数名を変更

--- include/robot.h
+++ include/robot.h
@@ -18,14 +18,14 @@ private:
 
 private:
     double interval;
-    State robot_state;
+    State state;
     Motor motor_r, motor_l;
     std::vector<double> sensor_value;
 
 public:
-    Robot(double interval = 0.01, State robot_state = {0.0, 0.0, 0.0}) :
+    Robot(double interval = 0.01, State initial_state = {0.0, 0.0, 0.0}) :
             interval(interval),
-            robot_state(robot_state),
+            state(initial_state),
             sensor_value(sensor_num),
             motor_r(interval),
             motor_l(interval){
@@ -39,9 +39,9 @@ private:
         double v = (speed_r + speed_l) / 2.0;
         double omega = (speed_r - speed_l) / width;
 
-        robot_state.x += v * interval * cos(robot_state.theta + omega * interval / 2.0);
-        robot_state.y += v * interval * sin(robot_state.theta + omega * interval / 2.0);
-        robot_state.theta += omega * interval;
+        state.x += v * interval * cos(state.theta + omega * interval / 2.0);
+        state.y += v * interval * sin(state.theta + omega * interval / 2.0);
+        state.theta += omega * interval;
         return;
     }
 
@@ -60,7 +60,7 @@ public:
     }
 
     State getState(){
-        return robot_state;
+        return state;
     }
 
     std::vector<double> getSensorData(){
@@ -70,11 +70,11 @@ public:
     void plot(){
         std::vector<double> x(6), y(6);
         for(int i = 0; i < 5; i++){
-            x.at(i) = robot_state.x + width * cos(robot_state.theta + i * M_PI / 2.0) / 2.0;
-            y.at(i) = robot_state.y + width * sin(robot_state.theta + i * M_PI / 2.0) / 2.0;
+            x.at(i) = state.x + width * cos(state.theta + i * M_PI / 2.0) / 2.0;
+            y.at(i) = state.y + width * sin(state.theta + i * M_PI / 2.0) / 2.0;
         }
-        x.at(5) = robot_state.x;
-        y.at(5) = robot_state.y;
+        x.at(5) = state.x;
+        y.at(5) = state.y;
 
         plt::plot(x, y);
         return;
