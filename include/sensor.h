2f9896d410dbca992e581cae24cd098af62004aa
ZyuC <m9t1k3zczc@gmail.com>
2020-04-20 23:37:05 +0900

    センサの観測点の位置を計算

--- include/sensor.h
+++ include/sensor.h
@@ -15,16 +15,33 @@ private:
 private:
     Position pos;
     std::vector<Line> lines;
+    std::vector<Position> observation_points;
     double value;
 
 public:
     Sensor():
             pos({0.0, 0.0}),
+            observation_points(r_num * theta_num),
             value(0.0){
+    }
+
+private:
+    void updateObservationPoint(){
+        for(int i = 0; i < theta_num; i++){
+            double theta = 2.0 * M_PI * i / theta_num;
+            for(int j = 0; j < r_num; j++){
+                double r = visible_radius * j / r_num;
+
+                observation_points.at(i * r_num + j).x = pos.x + r * cos(theta);
+                observation_points.at(i * r_num + j).y = pos.y + r * sin(theta);
+            }
+        }
+        return;
     }
 
 public:
     void update(){
+            updateObservationPoint();
         return;
     }
 
