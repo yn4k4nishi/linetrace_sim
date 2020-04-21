27fcaaa7de5bdbe210b3d9cc2b3a5a295638c9b4
ZyuC <m9t1k3zczc@gmail.com>
2020-04-22 00:31:31 +0900

    モータにバイアスを追加

--- include/noised_motor.h
+++ include/noised_motor.h
@@ -8,18 +8,21 @@ class NoisedMotor : public Motor{
 private:
     std::random_device seed;
     std::default_random_engine engine;
-    std::normal_distribution<> dist;
+    std::normal_distribution<> noise_dist, bias_dist;
+    double bias;
 
 public:
     NoisedMotor(double interval):
             Motor(interval),
             engine(seed()),
-            dist(1.0, 0.4){
+            noise_dist(1.0, 0.4),
+            bias_dist(1.0, 0.2),
+            bias(bias_dist(engine)){
     }
 
 public:
     double getSpeed(){
-        return angular_velocity / gear_ratio * tire_radius * dist(engine);
+        return angular_velocity / gear_ratio * tire_radius * noise_dist(engine) * bias;
     }
 };
 
