3a34d3697f842873ce583ee26cb3377c518308d9
ZyuC <m9t1k3zczc@gmail.com>
2020-04-22 00:28:47 +0900

    モータにノイズを追加

--- include/noised_motor.h
+++ include/noised_motor.h
@@ -1,17 +1,25 @@
 #ifndef LINETRACE_SIM_NOISED_MOTOR_H
 #define LINETRACE_SIM_NOISED_MOTOR_H
 
+#include <random>
 #include "motor.h"
 
 class NoisedMotor : public Motor{
+private:
+    std::random_device seed;
+    std::default_random_engine engine;
+    std::normal_distribution<> dist;
+
 public:
     NoisedMotor(double interval):
-            Motor(interval){
+            Motor(interval),
+            engine(seed()),
+            dist(1.0, 0.4){
     }
 
 public:
     double getSpeed(){
-        return angular_velocity / gear_ratio * tire_radius;
+        return angular_velocity / gear_ratio * tire_radius * dist(engine);
     }
 };
 
