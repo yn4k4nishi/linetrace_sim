7205672c3ff7c4d5cdcfa27bb1dfd0479266d640
ZyuC <m9t1k3zczc@gmail.com>
2020-04-24 17:23:40 +0900

    センサにノイズを追加

--- include/noised_sensor.h
+++ include/noised_sensor.h
@@ -1,9 +1,34 @@
 #ifndef LINETRACE_SIM_NOISED_SENSOR_H
 #define LINETRACE_SIM_NOISED_SENSOR_H
 
+#include <random>
 #include "sensor.h"
 
 class NoisedSensor : public Sensor{
+private:
+    const double noise_dev = 0.1;
+
+private:
+    std::random_device seed;
+    std::default_random_engine engine;
+    std::normal_distribution<> noise_dist;
+
+public:
+    NoisedSensor():
+            engine(seed()),
+            noise_dist(0.0, noise_dev){
+    }
+
+public:
+    double getValue(){
+        double v = value + noise_dist(engine);
+        if(v > 1.0){
+            v = 1.0;
+        }else if(v < 0.0){
+            v = 0.0;
+        }
+        return v;
+    }
 
 };
 
